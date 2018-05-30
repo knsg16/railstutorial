7c392cb35d6c694818aae11d01770db4d326203e
Yuuki Kanasugi <knsg16@gmail.com>
2018-05-30 15:37:20 +0000

    added heroku CLI

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app88574341@heroku.com'],
+    :password       => ENV['qt4l92qw2700'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
