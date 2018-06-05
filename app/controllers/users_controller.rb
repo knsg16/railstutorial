b893460af579821f6e7a1966324f39090f2cd44c
Yuuki Kanasugi <knsg16@gmail.com>
2018-06-05 23:32:03 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,20 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only:[:index,:edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user, only: :destroy
-  
-  
+
+
   def index
     @users = User.paginate(page: params[:page])
   end
-  
+
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -25,11 +26,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
-  def edit 
+
+  def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -39,41 +40,32 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User delete"
     redirect_to users_url
   end
-  
-  
+
+
    private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-    
+
     #before アクション
-    
-    #ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-    
+
     #正しいユーザーかどうか確認
    def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
    end
-   
+
    #管理者かどうか確認
    def admin_user
      redirect_to(root_url) unless current_user.admin?
    end
-    
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,12 +15,12 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "     "
     assert_not @user.valid?
   end
-  
+
   test "email should be present" do
     @user.email = "     "
     assert_not @user.valid?
   end
-  
+
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -30,7 +30,7 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-  
+
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
@@ -39,7 +39,7 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
-  
+
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
@@ -48,25 +48,33 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-  
+
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
-  
+
   test "password should be present(nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
-  
+
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = " " * 6
   end
-  
+
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 
 end
